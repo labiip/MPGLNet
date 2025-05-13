b1c0cd6abf781e45a6748cb86f7990fb27c02e99
IIPL-CUMT <67191892+labiip@users.noreply.github.com>
2025-05-13 11:15:08 +0800

    initialize2

---
+++ extensions/infocd/infoloss.py
@@ -0,0 +1,109 @@
+'''
+==============================================================
+
+    0-------------------------------0
+    |       Loss Functions          |
+    0-------------------------------0
+
+==============================================================
+
+    Compute chamfer distance loss L1/L2
+
+==============================================================
+'''
+
+import torch
+from me_utils.infocd.Chamfer3D.dist_chamfer_3D import chamfer_3DDist
+from models.CRAPCN_utils import fps_subsample
+
+chamfer_dist = chamfer_3DDist()
+
+def chamfer(p1, p2):
+    d1, d2, _, _ = chamfer_dist(p1, p2)
+    return torch.mean(d1) + torch.mean(d2)
+
+
+def chamfer_sqrt(p1, p2):
+    d1, d2, _, _ = chamfer_dist(p1, p2)
+    d1 = torch.clamp(d1, min=1e-9)
+    d2 = torch.clamp(d2, min=1e-9)
+    d1 = torch.mean(torch.sqrt(d1))
+    d2 = torch.mean(torch.sqrt(d2))
+    # import pdb; pdb.set_trace()
+    return (d1 + d2) / 2
+
+def chamfer_single_side(pcd1, pcd2):
+    d1, d2, _, _ = chamfer_dist(pcd1, pcd2)
+    d1 = torch.mean(d1)
+    return d1
+
+def chamfer_single_side_sqrt(pcd1, pcd2):
+    d1, d2, _, _ = chamfer_dist(pcd1, pcd2)
+    d1 = torch.clamp(d1, min=1e-9)
+    d2 = torch.clamp(d2, min=1e-9)
+    d1 = torch.mean(torch.sqrt(d1))
+    return d1
+
+def get_loss1(pcds_pred, partial, gt, sqrt=True):
+    # def get_loss_InfoV2(pcds_pred, partial, gt, sqrt=True):
+    """loss function
+    Args
+        pcds_pred: List of predicted point clouds, order in [Pc, P1, P2, P3...]
+    """
+    if sqrt:
+        CD = chamfer_sqrt
+        PM = chamfer_single_side_sqrt
+    else:
+        CD = chamfer
+        PM = chamfer_single_side
+
+    Pc, P1, P2, P3 = pcds_pred
+
+    gt_2 = fps_subsample(gt, P2.shape[1])
+    gt_1 = fps_subsample(gt_2, P1.shape[1])
+    gt_c = fps_subsample(gt_1, Pc.shape[1])
+
+    # cdc = CD(Pc, gt_c)
+    cdc = calc_cd_like_InfoV2(Pc, gt_c)
+    # cd1 = CD(P1, gt_1)
+    cd1 = calc_cd_like_InfoV2(P1, gt_1)
+    # cd2 = CD(P2, gt_2)
+    cd2 = calc_cd_like_InfoV2(P2, gt_2)
+    # cd3 = CD(P3, gt)
+    cd3 = calc_cd_like_InfoV2(P3, gt)
+
+    # partial_matching = PM(partial, P3)
+    # partial_matching = calc_cd_one_side_like_hyperV2(partial, P3)
+    partial_matching = calc_cd_one_side_like_InfoV2(partial, P3)
+
+    loss_all = (cdc + cd1 + cd2 + cd3 + partial_matching) * 1e3
+    losses = [cdc, cd1, cd2, cd3, partial_matching]
+    return loss_all, losses, [gt_2, gt_1, gt_c]
+
+
+def calc_cd_like_InfoV2(p1, p2):
+    dist1, dist2, idx1, idx2 = chamfer_dist(p1, p2)
+    dist1 = torch.clamp(dist1, min=1e-9)
+    dist2 = torch.clamp(dist2, min=1e-9)
+    d1 = torch.sqrt(dist1)
+    d2 = torch.sqrt(dist2)
+
+    distances1 = - torch.log(
+        torch.exp(-0.5 * d1) / (torch.sum(torch.exp(-0.5 * d1) + 1e-7, dim=-1).unsqueeze(-1)) ** 1e-7)
+    distances2 = - torch.log(
+        torch.exp(-0.5 * d2) / (torch.sum(torch.exp(-0.5 * d2) + 1e-7, dim=-1).unsqueeze(-1)) ** 1e-7)
+
+    return (torch.sum(distances1) + torch.sum(distances2)) / 2
+
+
+def calc_cd_one_side_like_InfoV2(p1, p2):
+    dist1, dist2, idx1, idx2 = chamfer_dist(p1, p2)
+    dist1 = torch.clamp(dist1, min=1e-9)
+    dist2 = torch.clamp(dist2, min=1e-9)
+    d1 = torch.sqrt(dist1)
+    d2 = torch.sqrt(dist2)
+
+    distances1 = - torch.log(
+        torch.exp(-0.5 * d1) / (torch.sum(torch.exp(-0.5 * d1) + 1e-7, dim=-1).unsqueeze(-1)) ** 1e-7)
+
+    return torch.sum(distances1)
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+HOME=`pwd`
+
+# Chamfer Distance
+cd $HOME/extensions/chamfer_dist
+python setup.py install --user
+
+# NOTE: For GRNet 
+
+# Cubic Feature Sampling
+cd $HOME/extensions/cubic_feature_sampling
+python setup.py install --user
+
+# Gridding & Gridding Reverse
+cd $HOME/extensions/gridding
+python setup.py install --user
+
+# Gridding Loss
+cd $HOME/extensions/gridding_loss
+python setup.py install --user
+
+# infocd
+cd $HOME/extensions/infocd/Chamfer3D
+python setup.py install --user
+
+
+
+
